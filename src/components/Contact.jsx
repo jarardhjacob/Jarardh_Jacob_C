75059590000a6736be3bb62835d90f75c72f2cd1
Git_JARARDH JACOB C <jerardhjcob@gmail.com>
2026-02-03 15:55:49 +0530

    updated contact secure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,9 +48,9 @@ const Contact = () => {
 
     setIsSending(true);
 
-    const SERVICE_ID = 'service_n5940q2';
-    const TEMPLATE_ID = 'template_4ghruad';
-    const PUBLIC_KEY = '9oSm1FavV0l__rCUV';
+    const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
     // Now correctly passing formRef.current (which points to the <form> element)
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, formRef.current, PUBLIC_KEY)
